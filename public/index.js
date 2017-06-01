6a0849a745ebb3711f404007bb25e454ca190982
Hugh Morrison <hmorri32@gmail.com>
2017-06-01 13:25:26 -0600

    create html architecture

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
-$(() => console.log('suh dude'));
+$(() => loadHoardedJunk());
+
+const loadHoardedJunk = () => {
+  fetch('/api/v1/goods')
+  .then(goods => goods.json());
+};
+
+class JunkFactory {
+  constructor(name, why_tho, cleanliness) {
+    this.name        = name;
+    this.why_tho     = why_tho;
+    this.cleanliness = cleanliness;
+  }
+}
