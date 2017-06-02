23cd0056f763e02774882660a1f8343c68af0121
Hugh Morrison <hmorri32@gmail.com>
2017-06-01 18:07:30 -0600

    config for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 module.exports = {
   development: {
     client: 'pg',
@@ -21,5 +23,17 @@ module.exports = {
       directory: './db/seeds/test'
     },
     useNullAsDefault: true
-  }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
 };
